cf8ec7ce6ac3f4592063a847537e3e1fe286230e
Giacomo Moscatelli <giacomom984@gmail.com>
2016-12-06 18:16:50 +0100

    Still working on search form and popup

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,14 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    if params[:search]
+      @items = Item.search(params[:search])
+    else
+      nil
+    end
   end
 
   def new
