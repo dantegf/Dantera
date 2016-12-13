8c08c9543b71ccc993770c971866adb852a03990
Dante Fereghetti <dante.fereghetti@gmail.com>
2016-12-13 13:10:05 +0100

    create working...kinda

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+
   def after_sign_in_path_for(resource)
     item_code = cookies[:search]
     if item_code
