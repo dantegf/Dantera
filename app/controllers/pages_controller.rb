5792bee568722b5a78d247aa6812a5033d500836
Dante Fereghetti <dante.fereghetti@gmail.com>
2017-03-30 18:53:41 -0500

    Updated routes, footer and footer css file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :how_to, :about_us ]
   def home
   end
+
+  def how_to
+  end
+
+  def about_us
+  end
+
 end
