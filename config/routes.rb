e5a077a40568fa249e80c5e59b8076f5e70265d4
Dante Fereghetti <dante.fereghetti@gmail.com>
2017-05-05 16:34:25 -0400

    Fixed email us

--- config/routes.rb
+++ config/routes.rb
@@ -33,12 +33,6 @@ Rails.application.routes.draw do
 
   get "/:search", to: "items#show"
 
-  # I (Dante) added this not sure if it's correct, but works /dantera.org/pages/about_us
-  get 'pages/how_to'
-
-  get 'pages/about_us'
-  # end
-
   # get 'items/index'
 
   # get 'items/new'
