32d71bc56aff079dc668289d47ec6d9313dc3625
Giacomo Moscatelli <giacomom984@gmail.com>
2017-05-13 14:19:28 +0200

    Browser always displays the top of the page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,11 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+  /* Browser always displays
+     the top of the page */
+  $('html, body').scrollTop(0);
+
+});
